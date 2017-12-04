99f1f82379d6ca4619bb0de5be7614c220ae5f73
clusterfiuba <cluster.fiuba@gmail.com>
2017-12-04 18:58:59 -0300

    add more examples

--- GravitationalWaves/gravitationalWaves.sh
+++ GravitationalWaves/gravitationalWaves.sh
@@ -3,8 +3,10 @@
 #SBATCH --job-name=gravitationalWaves
 #SBATCH --output=/scratch/labdac/gravitationalWaves_%A_%a.out
 #SBATCH --error=/scratch/labdac/gravitationalWaves_%A_%a.err
-#SBATCH --array=0-5%4
+#SBATCH --array=0-1%1
 #SBATCH --ntasks=1
 
+USERNAME=$1
+
 python gravitationalWaves.py $SLURM_ARRAY_TASK_COUNT $SLURM_ARRAY_TASK_ID
-cp result.dat /scratch/labdac/result_${SLURM_ARRAY_TASK_COUNT}_${SLURM_ARRAY_TASK_ID}.dat
+cp result.dat /scratch/$USERNAME/result_${SLURM_ARRAY_TASK_COUNT}_${SLURM_ARRAY_TASK_ID}.dat
--- GravitationalWaves/gravitationalWavesPostProc.py
+++ GravitationalWaves/gravitationalWavesPostProc.py
@@ -7,7 +7,8 @@ result = 0
 
 # Traigo los archivos de resultado del scratch
 for file in glob.glob(r'/scratch/labdac/result_*.dat'):
-    copy2(file, "/home/labdac")
+    copy2(file, ".")
+    print(file)
 
 for file in glob.glob(r'result_*.dat'):
 	tmp = np.loadtxt(file)
