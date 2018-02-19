3cab7b2057892065dcc3b8b7d759eba69992fe61
pablogmendez <pablo.guillermo.mendez@gmail.com>
2018-02-18 21:50:01 -0300

    remove examples for fix

--- gerris/simulation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/labdac/gerris/Gerris-ControllerModule/examples/cylinder_control_octave
-rm -fR vorticity.mpg
-rm -fR velocity.mpg
-rm -fR results/*
-/home/labdac/gerris/gerris-stable-custom/bin/gerris2D.sh -m cylinder_control.gfs 
